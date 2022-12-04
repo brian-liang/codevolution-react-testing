12bca9c3c888b38423f532292b89dc8404b0f621
brian-liang <brian.j.liang@gmail.com>
2022-12-03 19:35:08 -0700

    12 - Grouping Tests with describe

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,21 +1,29 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./greet"
 
-test('Greet renders correctly', () =>{
-    render(<Greet/>)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
+describe('Greet', () => {
+    test('Greet renders correctly', () =>{
+        render(<Greet/>)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+
+    describe('Nested', () => {
+
+        test('Greet renders with a name', () =>{
+            render(<Greet name="Brian"/>)
+            const textElement = screen.getByText("Hello Brian")
+            expect(textElement).toBeInTheDocument()
+        })
+    })
 })
+
+
 
 /*
 * Greet should render the text hello and if a name is passed into the component it should render hellow followed by the name
 */
 
-test('Greet renders with a name', () =>{
-    render(<Greet name="Brian"/>)
-    const textElement = screen.getByText("Hello Brian")
-    expect(textElement).toBeInTheDocument()
-})
 
 // test.only('Only runs this test', () =>{
 //     render(<Greet name="Brian"/>)
@@ -23,8 +31,8 @@ test('Greet renders with a name', () =>{
 //     expect(textElement).toBeInTheDocument()
 // })
 
-test.skip('Skipps test', () =>{
-    render(<Greet name="Brian"/>)
-    const textElement = screen.getByText("Hello Brian")
-    expect(textElement).toBeInTheDocument()
-})
+// test.skip('Skipps test', () =>{
+//     render(<Greet name="Brian"/>)
+//     const textElement = screen.getByText("Hello Brian")
+//     expect(textElement).toBeInTheDocument()
+// })
