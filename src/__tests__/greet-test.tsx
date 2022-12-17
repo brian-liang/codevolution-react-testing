ccfb79f10801a70c38b833bc265e2b53db9cd75c
brian-liang <brian.j.liang@gmail.com>
2022-12-16 21:00:23 -0700

    13 - Filename Conventions

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from "../components/greet/greet"
+
+describe('Greet', () => {
+    test('Greet renders correctly', () =>{
+        render(<Greet/>)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+
+    describe('Nested', () => {
+
+        test('Greet renders with a name', () =>{
+            render(<Greet name="Brian"/>)
+            const textElement = screen.getByText("Hello Brian")
+            expect(textElement).toBeInTheDocument()
+        })
+    })
+})
---
+++ src/components/greet/greet.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from "./greet"
+
+describe('Greet', () => {
+    it('Greet renders correctly', () =>{
+        render(<Greet/>)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+
+    describe('Nested', () => {
+
+        it('Greet renders with a name', () =>{
+            render(<Greet name="Brian"/>)
+            const textElement = screen.getByText("Hello Brian")
+            expect(textElement).toBeInTheDocument()
+        })
+    })
+})
